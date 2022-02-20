6a994d46711ffe4826421dd4e42f20cd37b692b8
samuVillegas <villegassamuel25@gmail.com>
2022-02-20 15:18:15 -0500

    Se limpia proyecto y se configura aplicacion "company"

--- Learningproject/movie/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
-
-def home(request):
-    return render(request, 'home.html', {'name': 'Pablo Maya'})
-    #return HttpResponse('<h1>Welcome to home page!</h1>')
-
