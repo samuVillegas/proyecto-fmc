7dfa762c2c6a3a1e381bc6115af9e07f5c974834
samuVillegas <villegassamuel25@gmail.com>
2022-02-24 08:45:30 -0500

    Se realiza maquetación inicial de creación de edificio

--- apps/company/urls.py
+++ apps/company/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index)
+    path('',views.index),
+    path('add_building',views.add_building),
+    path('search_building',views.search_building)
 ]
--- apps/company/views.py
+++ apps/company/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render, redirect
 # Create your views here
 def index(request):
     return render(request,"pages/index.html",{'message':'Hola mundo'})
+
+def add_building(request): 
+    return render(request,"pages/add_building.html")
+    
+def search_building(request): 
+    return render(request,"pages/search_building.html")
