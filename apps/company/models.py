ca8a83e376eab1968ca8ac9359b52578c9e57992
ivyteew <danilotoro112@gmail.com>
2022-03-04 23:31:25 -0500

    Implementacion de eliminar edificio, solucion de editar edificio y localstorage

--- apps/company/models.py
+++ apps/company/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 # Create your models here.
 class Building(models.Model):
-    code=models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    code=models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False,unique=True)
     site_name=models.CharField(max_length=50)
     address=models.CharField(max_length=70)
     contact_email=models.CharField(max_length=40)
     contact_mobile_number=models.PositiveIntegerField()
-    site_type=models.CharField(max_length=3, unique=True, blank=True, null=True, default=None)
+    site_type=models.CharField(max_length=3,unique=True, null=True, default=None)
