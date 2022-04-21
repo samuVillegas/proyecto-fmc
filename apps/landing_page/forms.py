c51dba63214eb3a42c1537d6d27d3c7c860034a2
pmayavi <pmayav@eafit.edu.co>
2022-04-21 09:16:37 -0500

    Mejora estetica del form

--- apps/landing_page/forms.py
+++ apps/landing_page/forms.py
@@ -1,19 +1,24 @@
 from email.policy import default
 from django import forms
-from apps.company.utilities.choose_type.Question import Question
-from apps.company.utilities.choose_type.Group import getQuestions
+from apps.company.utilities.choose_type.Question import Question 
 
 class ContactForm(forms.Form):
     def initials(self, questions):
         count = 1
+        size = 40
         for q in questions:
-            self.fields['lock' + str(count)] = forms.CharField(initial=q.lock)
-            self.fields['question' + str(count)] = forms.CharField(initial=q.question)
-            self.fields['image' + str(count)] = forms.CharField(initial=q.image)
+            self.fields['lock' + str(count)] = forms.CharField(initial=q.lock, 
+                widget=forms.Textarea(attrs={"rows":1, "cols":size/4, "style": "resize: none"}))
+            self.fields['question' + str(count)] = forms.CharField(initial=q.question, 
+                widget=forms.Textarea(attrs={"rows":(len(q.question)/size) + 1, "cols":size}))
+            self.fields['image' + str(count)] = forms.CharField(initial='\n'.join(q.image.split(';')), 
+                widget=forms.Textarea(attrs={"rows":len(q.image.split(';')) + 1, "cols":size}))
             count2 = 1
             for opt, out in zip(q.options, q.outputs):
-                self.fields['options' + str(count) + '_' + str(count2)] = forms.CharField(initial=opt)
-                self.fields['outputs' + str(count) + '_' + str(count2)] = forms.CharField(initial=out)
+                self.fields['options' + str(count) + '_' + str(count2)] = forms.CharField(initial=opt, 
+                    widget=forms.Textarea(attrs={"rows":len(opt)/size, "cols":size}))
+                self.fields['outputs' + str(count) + '_' + str(count2)] = forms.CharField(initial=out, 
+                    widget=forms.Textarea(attrs={"rows":len(out)/size, "cols":size}))
                 count2 += 1
             count += 1
 
