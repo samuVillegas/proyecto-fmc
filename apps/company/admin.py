3eb065555b7672fa869cab65b305d863c5f4c5ae
pmayavi <68768011+pmayavi@users.noreply.github.com>
2022-04-25 14:12:32 -0500

    Revert "Merge branch"

--- apps/company/admin.py
+++ apps/company/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from .models import Building, Inspection
+from .models import Building
 
 # Register your models here.
 admin.site.register(Building)
-admin.site.register(Inspection)
--- proyecto_fmc/urls.py
+++ proyecto_fmc/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('apps.landing_page.urls')),
-    path('company/',include('apps.company.urls')),
+    path('company/',include('apps.company.urls'))
 ]
