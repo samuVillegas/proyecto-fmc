c0e48522bba7a2d7e50e7c1e223d1eea941c871f
samuVillegas <villegassamuel25@gmail.com>
2022-05-04 23:40:56 -0500

    Se realiza una primera version del dashboard del usuari

---
+++ apps/administration/static/scripts/dashboard.js
@@ -0,0 +1,89 @@
+const URL = 'http://127.0.0.1:6060';
+
+const colors = [];
+for(let i = 0;i<100;i++){
+    colors.push(`rgba(${Math.floor((Math.random() * (255 - 0 + 1)) + 0)}, ${Math.floor((Math.random() * (255 - 0 + 1)) + 0)}, ${Math.floor((Math.random() * (255 - 0 + 1)) + 0)})`);
+}
+
+
+//Por cada gráfica se debe hacer una petición
+fetch(`${URL}/administration/dashboard_char_regulation/`).then(res => res.json()).then(res => {
+    const config = {
+        type: 'bar',
+        data: {
+            labels: ['Nacional', 'Internacional'],
+            datasets: [{
+                label: 'Edificios por normativa',
+                backgroundColor: colors,
+                borderColor: colors,
+                data: res
+            }]
+        },
+        options: {}
+    }
+    const ctx = document.getElementById('buildingsByRegulation').getContext('2d');
+    if (ctx !== null) {
+        const myChart = new Chart(
+            ctx,
+            config
+        );
+    }
+}).catch(err => err);
+
+fetch(`${URL}/administration/dashboard_buildings_by_type/`).then(res => res.json()).then(res => {
+    const data = res.map(info => info.cantidad);
+    const labels = res.map(info => info.site_type);
+
+    const config = {
+        type: 'doughnut',
+        data: {
+            labels,
+            datasets: [{
+                label: 'Edificios por tipo',
+                backgroundColor: colors,
+                borderColor: ['rgb(255, 99, 132)',
+                    'rgb(255, 159, 64)'],
+                data
+            }]
+        },
+        options: {}
+    }
+    const ctx = document.getElementById('buildingsByType').getContext('2d');
+    if (ctx !== null) {
+        const myChart = new Chart(
+            ctx,
+            config
+        );
+    }
+
+}).catch(err => err)
+
+
+
+fetch(`${URL}/administration/dashboard_building_inspection_state/`).then(res => res.json()).then(res => {
+    const config = {
+        type: 'line',
+        data: {
+            labels:['Aprobados','No Aprobados','Sin información'],
+            datasets: [{
+                label: 'Edificios por estado de inspección',
+                backgroundColor: colors,
+                borderColor: ['rgb(255, 99, 132)',
+                    'rgb(255, 159, 64)'],
+                data: res
+            }]
+        },
+        options: {}
+    }
+    const ctx = document.getElementById('buildingsByInspectionState').getContext('2d');
+    if (ctx !== null) {
+        const myChart = new Chart(
+            ctx,
+            config
+        );
+    }
+
+}).catch(err => err)
+
+
+
--- apps/administration/urls.py
+++ apps/administration/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('inspectors/',views.inspectors),
     path('buildings/',views.buildings),
     path('regulations/',views.regulations),
+    #Peticiones para el dashboard
+    path('dashboard_char_regulation/',views.dashboard_char_regulation),
+    path('dashboard_buildings_by_type/',views.dashboard_buildings_by_type),
+    path('dashboard_building_inspection_state/',views.dashboard_building_inspection_state)
 ]
