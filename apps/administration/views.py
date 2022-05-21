8e1a9d18d857d9ac7d8fdaaf0fd13dc06cc73322
pmayavi <pmayav@eafit.edu.co>
2022-05-21 16:01:09 -0500

    Detalles finales de estetica y arreglo de bugs

--- apps/administration/views.py
+++ apps/administration/views.py
@@ -5,6 +5,16 @@ from django.contrib.auth.models import User
 from django.db.models import Q
 from django.http import HttpResponse
 import json
+from django.shortcuts import redirect
+from apps.company.utilities.choose_type.Group import getQuestions
+from apps.company.utilities.data_flow.DataFlow import getQuestions as getQuestionsInsp
+from apps.company.utilities.input_request import get_building_information
+from apps.company.utilities.choose_type.Group import readFile as readFileType
+from apps.company.utilities.data_flow.DataFlow import readFile as readFileIns
+from apps.company.utilities.data_flow.Question import Question
+from apps.company.utilities.forms import ContactForm
+from apps.company.utilities.data_flow.DataFlow import getQuestionsFlow, writeFileFlow
+from apps.company.utilities.choose_type.Group import getQuestionsGroup, writeFileGroup
 # Create your views here.
 
 @login_required
@@ -65,5 +75,96 @@ def inspectors(request):
     return render(request,"pages/inspectors.html",{'inspectors':inspectors,'searchTerm':searchTerm})
 
 @login_required
-def regulations(request):
-    return render(request,"pages/regulations.html")
+def choose_regulation_to_show_admin(request):
+    return render(request, "pages/choose_regulation_to_show_admin.html")
+
+@login_required
+def show_regulation_information_admin(request, regulation, is_inspection_question):
+    questions = []
+    table_list = []
+    if is_inspection_question == '0':
+        dir = 'apps/company/utilities/choose_type'
+        questions = readFileType(dir + '/Group' + regulation + '.txt')
+
+        for ite,q in enumerate(questions):
+            if 'img' not in q.image:
+                string = q.image
+                questions[ite].image = string.split(';')
+            else: 
+                questions[ite].image = ''
+    else:
+        dir = 'apps/company/utilities/data_flow'
+        questions = readFileIns(dir + '/Flow' + regulation + '.txt')
+        #print(questions)
+
+        for ite,q in enumerate(questions):
+            if not isinstance(q,Question):
+                questions.pop(ite)
+        
+        for ite,r in enumerate(questions):
+            if isinstance(r,Question):
+                if 'img' not in r.image and r.image != '[]':
+                    string = r.image
+                    questions[ite].image = string.split(';')
+                else: 
+                    questions[ite].image = ''
+
+    return render(request, "pages/show_regulation_information_admin.html", {'questions':questions, 'is_inspection_question':is_inspection_question})
+
+@login_required
+def law_interface_admin(request):
+    return render(request, 'pages/law_interface_select_admin.html')
+
+@login_required
+def edit_law_admin(request, law):
+    form = ContactForm()
+    if 'Group' in law:
+        law = law.replace('Group','')
+        if request.method == 'POST':
+            dic = request.POST.dict()
+            if request.POST.get("addOption"):
+                form.addOption(dic)
+                return render(request, 'pages/law_interface_edit_group_admin.html', {'form': form})
+            elif request.POST.get("removeOption"):
+                form.removeOption(dic)
+                return render(request, 'pages/law_interface_edit_group_admin.html', {'form': form})
+            elif request.POST.get("addQuestion"):
+                form.addQuestion(dic)
+                return render(request, 'pages/law_interface_edit_group_admin.html', {'form': form})
+            elif request.POST.get("remove"):
+                form.remove(dic)
+                return render(request, 'pages/law_interface_edit_group_admin.html', {'form': form})
+            else:
+                writeFileGroup(law, dic)
+                return redirect('/administration/')
+        else:
+            questions = getQuestionsGroup(law)
+            form.initials(questions)
+            return render(request, 'pages/law_interface_edit_group_admin.html', {'form': form})
+
+    elif 'Flow' in law:
+        law = law.replace('Flow','')
+        if request.method == 'POST':
+            dic = request.POST.dict()
+            if request.POST.get("addOption"):
+                form.addOption(dic)
+                return render(request, 'pages/law_interface_edit_flow_admin.html', {'form': form})
+            elif request.POST.get("removeOption"):
+                form.removeOption(dic)
+                return render(request, 'pages/law_interface_edit_flow_admin.html', {'form': form})
+            elif request.POST.get("addQuestion"):
+                form.addQuestion(dic)
+                return render(request, 'pages/law_interface_edit_flow_admin.html', {'form': form})
+            elif request.POST.get("addFlow"):
+                form.addFlow(dic)
+                return render(request, 'pages/law_interface_edit_flow_admin.html', {'form': form})
+            elif request.POST.get("remove"):
+                form.remove(dic)
+                return render(request, 'pages/law_interface_edit_flow_admin.html', {'form': form})
+            else:
+                writeFileFlow(law, dic)
+                return redirect('/administration/')
+        else:
+            questions = getQuestionsFlow(law)
+            form.initials(questions)
+    return render(request, 'pages/law_interface_edit_flow_admin.html', {'form': form})
